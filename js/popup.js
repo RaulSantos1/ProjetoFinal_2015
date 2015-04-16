2548acea8d8a193f67eff53a3ef2fb3d4f8fb386
Raul Santos <rsraulsnts2gmail.com>
2015-04-16 12:28:45 +0100

    ooo

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+var U = "login.html";
+
+var X = 230;
+var Y = 200;
+var W = 530;
+var H = 565;
+var s="resizable,left="+X+",top="+Y+",screenX="+X+",screenY="+Y+",width="+W+",height="+H;
+function popMe(){
+var SGW = window.open(U,'TheWindow',s)
+}
