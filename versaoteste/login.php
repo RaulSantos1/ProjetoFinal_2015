dde76d857fa802f9b1104e90e07d0692ac3e9cad
Vasco Marques <vasco_marques_ei@hotmail.com>
2015-05-20 10:02:45 +0100

    pagina inicial form

--- versaoteste/login.php
+++ versaoteste/login.php
@@ -1,16 +1,42 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="windows-1252">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>TODO write content</div>
-    </body>
-</html>
+<?php
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+if (isset($_POST['add'])) {
+    $dbhost = 'localhost:3306';
+    $dbuser = 'adminsmartpeople';
+    $dbpass = 'smartpeople';
+    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+    if (!$conn) {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    if (!get_magic_quotes_gpc()) {
+        $v_mailsmartpeople = addslashes($_POST['mailsmartpeople']);
+        $v_passwordsmartpeople = addslashes($_POST['passwordsmartpeople']);
+        $v_mailfacebook = addslashes($_POST['mailfacebook']);
+        $v_passwordfacebook = addslashes($_POST['passwordfacebook']);
+        $v_maillinkdin = addslashes($_POST['maillinkdin']);
+        $v_passwordlinkdin = addslashes($_POST['passwordlinkdin']);
+    } else {
+        $v_mailsmartpeople = $_POST['mailsmartpeople'];
+        $v_passwordsmartpeople = $_POST['passwordsmartpeople'];
+        $v_mailfacebook = addslashes($_POST['mailfacebook']);
+        $v_passwordfacebook = addslashes($_POST['passwordfacebook']);
+        $v_maillinkdin = addslashes($_POST['maillinkdin']);
+        $v_passwordlinkdin = addslashes($_POST['passwordlinkdin']);
+    }
+
+
+    $sql = "INSERT INTO smartpeople.registos " .
+            "(mailsmartpeople,passwordsmartpeople,mailfacebook,passwordfacebook,maillinkdin,passwordlinkdin) " .
+            "VALUES('$v_mailsmartpeople','$v_passwordsmartpeople','$v_mailfacebook','$v_passwordfacebook','$v_maillinkdin','$v_passwordlinkdin') ";
+    mysql_select_db('smartpeople');
+    $retval = mysql_query($sql, $conn);
+    if (!$retval) {
+        die('Could not enter data: ' . mysql_error());
+    }
+
+    header("Location: http://localhost/indexlogin.html");
+    mysql_close($conn);
+}
+?>
