09ceeae374adc67c16dbf96fb340b87f8a563f15
Vasco Marques <vasco_marques_ei@hotmail.com>
2015-05-20 19:48:22 +0100

    login & logout

--- versaoteste/user.php
+++ versaoteste/user.php
@@ -1,6 +1,6 @@
 <?php
 // incluir o arquivo de ligação com a base de dados
-$connect = mysql_connect('localhost', 'adminsmartpeople', 'smartpeople');
+$connect = mysql_connect('localhost','adminsmartpeople','smartpeople');
 $db = mysql_select_db('smartpeople');
 
 // busca de dados em ordem decrescente (entrada mais recente primeiro)
@@ -11,16 +11,14 @@ $result = mysql_query("SELECT * FROM registos ORDER BY id DESC");
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Cake Design</title>
-        <link rel="stylesheet" type="text/css" href="css/trabalho_final.css">
-        <link rel="icon" type="image/png" href="images/logotipo.png"/>
-        <link rel="index" type="text/javascript" href="LOGIN/index.php">
+        <title>SmartPeople - user</title>
+        <link rel="index" type="text/javascript" href="versaoteste1/index.php">
         <link rel="stylesheet" type="text/javascript" href="versaoteste1/login.php">
 
     </head>
     <body>
         <?php
-        $username_cookie = $_SESSION['mailsmartpeople'];
+        $username_cookie = $_COOKIE['mailsmartpeople'];
         if (isset($username_cookie)) {
             echo"<b>Bem-Vindo</b>, $username_cookie <br>";
             echo"Estas informações <font color='red'>PODEM</font> ser visualizadas por si";
@@ -33,5 +31,4 @@ $result = mysql_query("SELECT * FROM registos ORDER BY id DESC");
         ?>
 
 
-    </body>
-</html>
+    </body></html>
